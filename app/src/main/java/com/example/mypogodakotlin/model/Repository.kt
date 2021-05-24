c3595aba0234c3b5b21904842eabdc74a309c254
android <https://github.com/Tortoewka/android.git>
2021-05-24 19:29:22 +0500

    lesson2

---
+++ app/src/main/java/com/example/mypogodakotlin/model/Repository.kt
@@ -0,0 +1,7 @@
+package com.example.mypogodakotlin.model
+
+interface Repository {
+    fun getWeatherFromServer(): Weather
+    fun getWeatherFromLocalStorageRus(): List<Weather>
+    fun getWeatherFromLocalStorageWorld(): List<Weather>
+}
---
+++ app/src/main/java/com/example/mypogodakotlin/model/dfgfdg.kt
@@ -0,0 +1,4 @@
+package com.example.mypogodakotlin.model
+
+class dfgfdg {
+}
